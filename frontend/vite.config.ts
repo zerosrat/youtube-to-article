9d5c64bd544cc3118d36a2d95b2a8316b0fd39e1
zerosrat <zerosrat@163.com>
2026-06-03 23:31:21 +0800

    chore: initialize frontend with vite

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8787',
+        changeOrigin: true
+      }
+    }
+  }
+})
